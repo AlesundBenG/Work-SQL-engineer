9fa15fbc1d5e8716b867129df945876f4359a37f
Паша <77670636+AlesundBenG@users.noreply.github.com>
2021-06-25 09:47:40 +0300

    Закрытие более одной действующей ИПРА в ЛД
    Добавление формирования мероприятий для выгрузки в витрину.

--- ИЗМЕНЕНИЯ В БАЗЕ/Закрытие более одной действующей ИПРА в ЛД/Закрытие более одной действующей ИПРА в ЛД.sql
+++ ИЗМЕНЕНИЯ В БАЗЕ/Закрытие более одной действующей ИПРА в ЛД/Закрытие более одной действующей ИПРА в ЛД.sql
@@ -120,6 +120,45 @@ WHERE END_DATE IS NULL
 --------------------------------------------------------------------------------------------------------------------------------
 
 
+--Формирование мероприятий для отправки в витрину, если они еще не сформированы.
+INSERT INTO WM_SOCIAL_REHABILITATION (A_GUID,/*A_CROWNER*/A_TS, A_STATUS, A_STATUS_EVENT_IPRA,/*A_EDITOR*/A_CREATEDATE, A_DATE_START, A_DATE_END, A_REHAB_REF, A_NEED, A_RHB_TYPE, A_RHB_EVNT)
+SELECT 
+    NEWID()                     AS A_GUID,					---Глобальный идентификатор
+    GETDATE()                   AS A_TS,					---Дата модификации
+    10                          AS A_STATUS,				---Статус в БД
+    1                           AS A_STATUS_EVENT_IPRA,		---Статус мероприятия ИПРА
+    GETDATE()                   AS A_CREATEDATE,			---Дата создания
+    Docs.ISSUEEXTENSIONSDATE    AS A_DATE_START,			---Дата начала
+    Docs.COMPLETIONSACTIONDATE  AS A_DATE_END,				---Дата окончания
+    Ipra.OUID                   AS A_REHAB_REF,				---Реабилитационные мероприятия
+    1                           AS A_NEED,					---Нуждается
+    Reh.A_RHB_TYPE              AS A_RHB_TYPE,				---Тип мероприятия
+    SubTypeReh.A_OUID           AS A_RHB_EVNT				---Подтип мероприятия
+FROM WM_REH_REFERENCE Ipra --Реабилитационные мероприятия по заболеванию
+----Действующие документы
+    INNER JOIN WM_ACTDOCUMENTS Doc
+        ON Ipra.A_IPR = Doc.OUID 
+            AND Doc.A_STATUS=10     --Документ не удален из БД
+----Рекомендованные мероприятия
+	INNER JOIN WM_SOCIAL_REHABILITATION Reh
+        ON Reh.A_REHAB_REF=Ipra.OUID       
+            and Reh.A_STATUS=10                 --Статус в БД - действует
+            AND Reh.A_STATUS_EVENT_IPRA is NULL --Нет статуса мероприятия
+            AND Reh.A_RHB_EVNT is NULL          --Не указан подтип мероприятия
+----Подтипы мероприятий из справочника ЕСРН
+		LEFT JOIN SPR_subTYPE_SOC_REHUB SubTypeReh
+			ON SubTypeReh.a_type_of_event = Reh.A_RHB_TYPE
+			    AND SubTypeReh.a_type_of_event IN (20, 21, 18, 17)
+----Документ ИПРА.
+    INNER JOIN WM_ACTDOCUMENTS Docs
+        ON Ipra.A_IPR =Docs.ouid
+WHERE Ipra.A_STATUS=10 ---ИПРА не удалена из БД
+    AND Ipra.OUID IN (SELECT IPRA_OUID FROM #IPRA_FOR_CLOSE) --Вставляем в те, которые собираемся закрывать.
+
+
+--------------------------------------------------------------------------------------------------------------------------------
+
+
 --Мероприятия социальной реабилитации для обработки.
 INSERT INTO #REHABILITATION_FOR_CLOSE (REHABILITATION_OUID, IPRA_OUID, PERSONOUID)
 SELECT 
@@ -145,8 +184,8 @@ SET rehabilitation.A_ORG = rehabilitation.A_REC_ORG,
     rehabilitation.A_REASON = CASE WHEN A_RHB_TYPE = 17 THEN 'Выполнение мероприятий по социально-средовой реабилитации не входит в компетенцию министерства соц. развития Кировской области' ELSE NULL END,
     rehabilitation.A_NOTE =  'Органами МСЭ взамен настоящей ИПРА выдана новая',
     rehabilitation.A_TS = GETDATE(),
-    rehabilitation.A_EDITOR = #curAccount#
-    --rehabilitation.A_EDITOR = 10314303
+    --rehabilitation.A_EDITOR = #curAccount#
+    rehabilitation.A_EDITOR = 10314303
 FROM WM_SOCIAL_REHABILITATION rehabilitation
 	 INNER JOIN #REHABILITATION_FOR_CLOSE forClose
 	    ON forClose.REHABILITATION_OUID = rehabilitation.A_OUID
@@ -156,8 +195,8 @@ UPDATE actDocuments
 SET actDocuments.A_DOCSTATUS = 5,
     actDocuments.COMPLETIONSACTIONDATE = forClose.END_DATE, 
     actDocuments.TS = GETDATE(), 
-    actDocuments.A_EDITOWNER = #curAccount#
-    --actDocuments.A_EDITOWNER = 10314303
+    --actDocuments.A_EDITOWNER = #curAccount#
+    actDocuments.A_EDITOWNER = 10314303
 FROM WM_ACTDOCUMENTS actDocuments
     INNER JOIN #IPRA_FOR_CLOSE forClose
         ON forClose.DOCUMENT_OUID = actDocuments.OUID
@@ -170,9 +209,38 @@ WHERE actDocuments.OUID IN (
             ON rehabilitation.A_REHAB_REF = forClose.IPRA_OUID
     WHERE rehabilitation.A_STATUS = 10 --Статус в БД "Действует".
     GROUP BY forClose.PERSONOUID, forClose.IPRA_OUID, forClose.DOCUMENT_OUID, forClose.START_DATE, forClose.END_DATE
-    HAVING MAX(CASE WHEN rehabilitation.A_STATUS_EVENT_IPRA = 1 THEN 1 ELSE 0 END) = 1                      --Есть сформированные мероприятия.
-        OR COUNT(*) = SUM(CASE WHEN ISNULL(rehabilitation.A_STATUS_EVENT_IPRA, 4) = 4 THEN 1 ELSE 0 END)    --Либо все выгружены в витрину.
-        OR COUNT(*) = SUM(CASE WHEN ISNULL(rehabilitation.A_STATUS_EVENT_IPRA, 3) = 3 THEN 1 ELSE 0 END)    --Либо все на выгрузку в витрину.
+    HAVING MAX(CASE WHEN rehabilitation.A_STATUS_EVENT_IPRA = 1 THEN 1 ELSE 0 END) = 1          --Есть сформированные мероприятия.
+        OR COUNT(*) = SUM(CASE WHEN ISNULL(rehabilitation.A_STATUS_EVENT_IPRA, 4) = 4 THEN 1 ELSE 0 END)   --Либо все выгружены в витрину.
+        OR COUNT(*) = SUM(CASE WHEN ISNULL(rehabilitation.A_STATUS_EVENT_IPRA, 3) = 3 THEN 1 ELSE 0 END)   --Либо все на выгрузку в витрину.
+)
+
+--Фиксация изменений.
+INSERT INTO TEMPORARY_TABLE(VARCHAR_1, VARCHAR_2, VARCHAR_3, VARCHAR_4, VARCHAR_5, VARCHAR_6, VARCHAR_7, VARCHAR_8, VARCHAR_10)
+SELECT
+    'Дата выполнения:'              AS VARCHAR_1,
+    GETDATE()                       AS VARCHAR_2,
+    'Идентификатор ИПРА:'           AS VARCHAR_3,
+    forClose.IPRA_OUID              AS VARCHAR_4,
+    'Идентификатор документа ИПРА:' AS VARCHAR_5,
+    forClose.DOCUMENT_OUID          AS VARCHAR_6,
+    'Идентификатор личного дела:'   AS VARCHAR_7,
+    forClose.PERSONOUID             AS VARCHAR_8,
+    'Закрытие двойных ИПРА'         AS VARCHAR_10
+FROM WM_ACTDOCUMENTS actDocuments
+    INNER JOIN #IPRA_FOR_CLOSE forClose
+        ON forClose.DOCUMENT_OUID = actDocuments.OUID
+WHERE actDocuments.OUID IN (
+    SELECT 
+        forClose.DOCUMENT_OUID
+    FROM WM_SOCIAL_REHABILITATION rehabilitation --Мероприятие социальной реабилитации.
+    ----ИПРА для закрытия.
+        INNER JOIN #IPRA_FOR_CLOSE forClose 
+            ON rehabilitation.A_REHAB_REF = forClose.IPRA_OUID
+    WHERE rehabilitation.A_STATUS = 10 --Статус в БД "Действует".
+    GROUP BY forClose.PERSONOUID, forClose.IPRA_OUID, forClose.DOCUMENT_OUID, forClose.START_DATE, forClose.END_DATE
+    HAVING MAX(CASE WHEN rehabilitation.A_STATUS_EVENT_IPRA = 1 THEN 1 ELSE 0 END) = 1          --Есть сформированные мероприятия.
+        OR COUNT(*) = SUM(CASE WHEN ISNULL(rehabilitation.A_STATUS_EVENT_IPRA, 4) = 4 THEN 1 ELSE 0 END)   --Либо все выгружены в витрину.
+        OR COUNT(*) = SUM(CASE WHEN ISNULL(rehabilitation.A_STATUS_EVENT_IPRA, 3) = 3 THEN 1 ELSE 0 END)   --Либо все на выгрузку в витрину.
 )
 
 
@@ -208,5 +276,3 @@ WHERE actDocuments.OUID IN (
         OR COUNT(*) = SUM(CASE WHEN ISNULL(rehabilitation.A_STATUS_EVENT_IPRA, 3) = 3 THEN 1 ELSE 0 END)   --Либо все на выгрузку в витрину.
 )
 order by 1
-
-
